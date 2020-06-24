7e162ea70a903b9f64d5e392edc9a1a65a20282f
SiddharthMehta <siddharthmehta1234@gmail.com>
2020-06-24 19:17:57 +0530

    Model Building

--- Model Building/model_building.py
+++ Model Building/model_building.py
@@ -28,13 +28,34 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 #Multiple linear regression (Linear model)
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
-lr=LinearRegression()
-print(cross_val_score(lr, X, y, cv=3, scoring='neg_mean_absolute_error '))
+lm=LinearRegression()
+lm.fit(X, y)
+print(cross_val_score(lm, X_train, y_train, cv=3, scoring='neg_mean_absolute_error').mean())
 
-#Lasso regression (Linear model)
 
 #Random Forest (tree-based)
+from sklearn.ensemble import RandomForestRegressor
+rf=RandomForestRegressor()
+rf.fit(X_train, y_train)
 
-#Tune modelGridSearchCV
+print(cross_val_score(rf, X_train, y_train, cv=3, scoring='neg_mean_absolute_error').mean())
 
-#test
+#Tune using modelGridSearchCV
+from sklearn.model_selection import GridSearchCV
+parameters = {'n_estimators':range(10, 300, 10), 'criterion':("mse", "mae"), 'max_features':('auto', 'sqrt', 'log2')}
+gs=GridSearchCV(rf, parameters, scoring='neg_mean_absolute_error', cv=3)
+gs.fit(X_train, y_train)
+
+gs.best_score_
+gs.best_estimator_
+
+
+#testing the oredicted and actual values for different model
+tpred_lm=lm.predict(X_test)
+tpred_rf=rf.predict(X_test)
+tpred_best_rf=gs.best_estimator_.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error
+print(f'MEA for Linear Regression Model is: {mean_absolute_error(y_test, tpred_lm)}')
+print(f'MEA for a Random Forest is: {mean_absolute_error(y_test, tpred_rf)}')
+print(f'MAE for Random Forest with Model Tuning is: {mean_absolute_error(y_test, tpred_best_rf)}')
